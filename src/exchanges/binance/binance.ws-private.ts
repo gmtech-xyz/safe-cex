427cf970cdcb27c2156a854204cdb26b73993689
aalwayslucky <73863117+aalwayslucky@users.noreply.github.com>
2024-04-16 17:56:14 +0700

    Remove Expired Orders from store
    we need to remove expired ro orders because we can't have more than 100% of pos in ro orders so they got expired

--- src/exchanges/binance/binance.ws-private.ts
+++ src/exchanges/binance/binance.ws-private.ts
@@ -88,7 +88,7 @@ export class BinancePrivateWebsocket extends BaseWebSocket<BinanceExchange> {
         });
       }
 
-      if (data.X === 'CANCELED' || data.X === 'FILLED') {
+      if (data.X === 'CANCELED' || data.X === 'FILLED' || data.X === 'EXPIRED') {
         this.store.removeOrder({ id: data.c });
       }
     });
